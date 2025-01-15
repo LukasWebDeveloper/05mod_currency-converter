a4ff86e02f7f50588487805736fa60ea0ddfe421
Lukasz Duszak <lukas94.webdeveloper@gmail.com>
2025-01-15 20:00:48 +0100

    commit update files

--- js/script.js
+++ js/script.js
@@ -1,46 +1,48 @@
-let formElement = document.querySelector(".js-form");                // formularz
-let currency_amount = document.querySelector(".js-currencyAmount");  // wprowadzana kwota pieniędzy w PLN
-let rate_amount = document.querySelector(".js-rateAmount");          // zamieniana kwota w PLN
-let currencyElement = document.querySelector(".js-currency")         // nowa waluta
-let final_amount = document.querySelector(".js-finalAmount");        // ostateczna zamieniona kwota waluty
+{
+   const formElement = document.querySelector(".js-form");                // formularz
+   const currency_amount = document.querySelector(".js-currencyAmount");  // wprowadzana kwota pieniędzy w PLN
+   const rate_amount = document.querySelector(".js-rateAmount");          // zamieniana kwota w PLN
+   const currencyElement = document.querySelector(".js-currency")         // nowa waluta
+   const final_amount = document.querySelector(".js-finalAmount");        // ostateczna zamieniona kwota waluty
 
-let plnRate = 1;
-let eurRate = 4.23;
-let gbpRate = 5.13;
-let usdRate = 4.09;
-let chfRate = 4.53;
+   const plnRate = 1;
+   const eurRate = 4.23;
+   const gbpRate = 5.13;
+   const usdRate = 4.09;
+   const chfRate = 4.53;
 
-formElement.addEventListener("input", () => {
+   formElement.addEventListener("input", () => {
 
-   let amount = +currency_amount.value;
-   let currency = currencyElement.value;
+      const amount = +currency_amount.value;
+      const currency = currencyElement.value;
 
-   let result;
+      let result;
 
 
-   switch (currency) {
-      case "EUR":
-         result = amount / eurRate;
-         break;
+      switch (currency) {
+         case "EUR":
+            result = amount / eurRate;
+            break;
 
-      case "PLN":
-         result = amount / plnRate;
-         break;
+         case "PLN":
+            result = amount / plnRate;
+            break;
 
-      case "GBP":
-         result = amount / gbpRate;
-         break;
+         case "GBP":
+            result = amount / gbpRate;
+            break;
 
-      case "USD":
-         result = amount / usdRate;
-         break;
+         case "USD":
+            result = amount / usdRate;
+            break;
 
-      case "CHF":
-         result = amount / chfRate;
-         break;
-   }
+         case "CHF":
+            result = amount / chfRate;
+            break;
+      }
 
-   rate_amount.innerHTML = `${currency_amount.value} zł`;
+      rate_amount.innerHTML = `${currency_amount.value} zł`;
 
-   final_amount.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-});
+      final_amount.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+   });
+}
