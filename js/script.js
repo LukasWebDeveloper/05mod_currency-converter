fdfca507dec7c7c455dd4186adacd66cb1be5a4d
Lukasz Duszak <lukas94.webdeveloper@gmail.com>
2025-01-17 19:59:19 +0100

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
 {
-   const formElement = document.querySelector(".js-form");                // formularz
-   const currency_amount = document.querySelector(".js-currencyAmount");  // wprowadzana kwota pieniędzy w PLN
-   const rate_amount = document.querySelector(".js-rateAmount");          // zamieniana kwota w PLN
-   const currencyElement = document.querySelector(".js-currency")         // nowa waluta
-   const final_amount = document.querySelector(".js-finalAmount");        // ostateczna zamieniona kwota waluty
 
-   const plnRate = 1;
-   const eurRate = 4.23;
-   const gbpRate = 5.13;
-   const usdRate = 4.09;
-   const chfRate = 4.53;
 
-   formElement.addEventListener("input", () => {
 
-      const amount = +currency_amount.value;
-      const currency = currencyElement.value;
+   const calculateResult = (amount, currency) => {
 
-      let result;
-
+      const plnRate = 1;
+      const eurRate = 4.23;
+      const gbpRate = 5.13;
+      const usdRate = 4.09;
+      const chfRate = 4.53;
 
       switch (currency) {
          case "EUR":
@@ -41,8 +32,39 @@
             break;
       }
 
+   }
+
+   const updateResultText = (amount, result, currency) => {
+      const final_amount = document.querySelector(".js-finalAmount");        // ostateczna zamieniona kwota waluty
+      const rate_amount = document.querySelector(".js-rateAmount");          // zamieniana kwota w PLN
+
+
       rate_amount.innerHTML = `${currency_amount.value} zł`;
 
       final_amount.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-   });
+   }
+
+   const onFormSubmit = (event) => {
+      event.preventDefault()
+
+      const currency_amount = document.querySelector(".js-currencyAmount");  // wprowadzana kwota pieniędzy w PLN
+      const currencyElement = document.querySelector(".js-currency")         // nowa waluta
+
+      const amount = +currency_amount.value;
+      const currency = currencyElement.value;
+
+      const result = calculateResult(amount, currency);
+
+
+      updateResultText(amount, result, currency);
+   }
+
+
+   const init = () => {
+      const formElement = document.querySelector(".js-form");                // formularz
+
+      formElement.addEventListener("submit", onFormSubmit);
+   }
+
+   init();
 }
